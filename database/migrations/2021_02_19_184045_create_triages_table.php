a235292322027c9915219615410e0c4dedd9346e
yangpanda <ypp13222204208@gmail.com>
2021-02-19 18:47:05 +0800

    2.19

---
+++ database/migrations/2021_02_19_184045_create_triages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTriagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('triages', function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger('minor_wound')->default(0)->comment('轻伤');
+            $table->tinyInteger('moderate_wound')->default(0)->comment('中度伤');
+            $table->tinyInteger('serious_injuries')->default(0)->comment('重伤');
+            $table->tinyInteger('death')->default(0)->comment('死亡');
+            $table->unsignedInteger('user_id')->comment('用户id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('triages');
+    }
+}
